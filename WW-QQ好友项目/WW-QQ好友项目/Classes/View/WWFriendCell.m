1c3b978c241551eb00141e3401e905056f3932ed
huangwenmei <123@qq.com>
2015-11-14 21:57:29 +0800

    简单实现 -> xib自定义cell

--- WW-QQ好友项目/WW-QQ好友项目/Classes/View/WWFriendCell.m
+++ WW-QQ好友项目/WW-QQ好友项目/Classes/View/WWFriendCell.m
@@ -8,26 +8,45 @@
 
 #import "WWFriendCell.h"
 
+@interface WWFriendCell ()
+@property (weak, nonatomic) IBOutlet UIImageView *iconImage;
+
+@property (weak, nonatomic) IBOutlet UILabel *nameLB;
+@property (weak, nonatomic) IBOutlet UILabel *detailNameLB;
+
+@end
+
 @implementation WWFriendCell
 
 + (instancetype)friendCellWithTableView:(UITableView *)tableView {
     
-    NSString *ID = @"cell";
-    WWFriendCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
-    if (!cell) {
-        cell = [[WWFriendCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:ID];
-    }
+//    NSString *ID = @"cell";
+//    WWFriendCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+//    if (!cell) {
+//        cell = [[WWFriendCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:ID];
+//    }
+    WWFriendCell *cell = [[[NSBundle mainBundle] loadNibNamed:@"WWFriendCell" owner:self options:nil] lastObject];
+    
     return cell;
 }
 
 - (void)setFriendData:(WWFriends *)friendData {
     _friendData = friendData;
-    self.imageView.image = [UIImage imageNamed:friendData.icon];
-    self.textLabel.text = friendData.name;
-    self.textLabel.textColor = friendData.isVip ? [UIColor orangeColor] : [UIColor blackColor];
-    self.detailTextLabel.text = friendData.intro;
+//    self.imageView.image = [UIImage imageNamed:friendData.icon];
+//    self.textLabel.text = friendData.name;
+//    self.textLabel.textColor = friendData.isVip ? [UIColor orangeColor] : [UIColor blackColor];
+//    self.detailTextLabel.text = friendData.intro;
+    
+    self.iconImage.image = [UIImage imageNamed:friendData.icon];
+    self.nameLB.text = friendData.name;
+    self.nameLB.textColor = friendData.isVip ? [UIColor orangeColor] : [UIColor blackColor];
+    self.detailNameLB.text = friendData.intro;
     
 }
+
+//+ (void)load {
+//    [[NSBundle mainBundle] loadNibNamed:@"WWFriendCell.plist" owner:self options:nil];
+//}
 
 
 
